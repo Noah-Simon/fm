855836a044ffbb40a1eb0123d3f03ba8bdac6939
yelqgd <1009280818@qq.com>
2017-09-19 21:49:54 +0800

    modify an url and npm build

--- src/urls.js
+++ src/urls.js
@@ -1,3 +1,3 @@
 export const CHANNELS_URL = 'https://jirenguapi.applinzi.com/fm/getChannels.php'
 export const SONGS_URL = 'https://jirenguapi.applinzi.com/fm/getSong.php?channel='
-export const LRCS_URL = 'http://jirenguapi.applinzi.com/fm/getLyric.php?&sid='
+export const LRCS_URL = 'https://jirenguapi.applinzi.com/fm/getLyric.php?&sid='
